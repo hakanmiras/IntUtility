a8f7f6a60d2e5bbc6bdb4fe5c40f1ebb2ead7cee
Hakan Mirasoğlu <hakan.mirasoglu@flo.com.tr>
2024-11-23 23:40:51 +0300

    convert seconds

--- Sources/IntUtility/IntUtility.swift
+++ Sources/IntUtility/IntUtility.swift
@@ -3,8 +3,12 @@
 
 
 extension Int {
-    public var biggestNum: Int {
-        return $0 + $1
+    func secondsToHoursMinutesSeconds() -> String {
+        let (h, m, s) = (self / 3600, (self % 3600) / 60, (self % 3600) % 60) // swiftlint:disable:this identifier_name
+        
+        let h_string = h < 10 ? "0\(h)" : "\(h)" // swiftlint:disable:this identifier_name
+        let m_string = m < 10 ? "0\(m)" : "\(m)" // swiftlint:disable:this identifier_name
+        let s_string = s < 10 ? "0\(s)" : "\(s)" // swiftlint:disable:this identifier_name
+        return "\(h_string):\(m_string):\(s_string)"
     }
-    
 }
